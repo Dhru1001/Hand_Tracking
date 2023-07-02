17549d920b30438e3f3eedac6b2b21524ef71dfa
Dhrumil Shah <92982318+Dhru1001@users.noreply.github.com>
2023-07-02 18:29:57 +0530

    Delete tp.py

--- tp.py
+++
@@ -1,6 +0,0 @@
-from mediapipe.python._framework_bindings.packet import Packet
-
-# You can now use the Packet class in your code
-packet = Packet()
-
-# Perform operations with the Packet object as needed
