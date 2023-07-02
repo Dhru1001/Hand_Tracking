4d8119295fa7b353c3dcde1c41d3fc92c3051ebb
Dhrumil Shah <dhrumilshah1001@gmail.com>
2023-07-02 17:23:38 +0530

    Add initial files for hand tracking project

---
+++ tp.py
@@ -0,0 +1,6 @@
+from mediapipe.python._framework_bindings.packet import Packet
+
+# You can now use the Packet class in your code
+packet = Packet()
+
+# Perform operations with the Packet object as needed
